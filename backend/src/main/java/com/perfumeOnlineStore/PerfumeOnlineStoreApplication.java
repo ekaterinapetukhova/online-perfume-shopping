2123a4f1d4e917397de1e0a3d21e359670b8ae62
Ekaterina Petukhova <katepthv12@gmail.com>
2024-07-17 08:55:04 +0200

    feat: add backend template file structure

---
+++ backend/src/main/java/com/perfumeOnlineStore/PerfumeOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.perfumeOnlineStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeOnlineStoreApplication.class, args);
+	}
+
+}
