8f11058b922186d29c6b15b789dc3e881ae7d1c0
Ekaterina Petukhova <katepthv12@gmail.com>
2024-07-23 13:39:39 +0200

    feat: implement ProductDto

---
+++ backend/src/main/java/com/perfumeOnlineStore/dto/ProductDto.java
@@ -0,0 +1,22 @@
+package com.perfumeOnlineStore.dto;
+
+import com.perfumeOnlineStore.entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String brand;
+    private Double price;
+    private String components;
+    private String scentGroups;
+    private Integer quantity;
+    private Product.Gender gender;
+    private Integer volume;
+}
