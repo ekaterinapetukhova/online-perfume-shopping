2f9859c0adc39a5518afbbc9d8c6e52cb0393229
Ekaterina Petukhova <katepthv12@gmail.com>
2024-08-13 08:39:26 +0200

    refactor: add repository annotation for RefreshTokenRepository and add findByToken mthod

--- backend/src/main/java/com/perfumeOnlineStore/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/perfumeOnlineStore/repository/RefreshTokenRepository.java
@@ -2,8 +2,11 @@ package com.perfumeOnlineStore.repository;
 
 import com.perfumeOnlineStore.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.*;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByToken(String token);
 }
