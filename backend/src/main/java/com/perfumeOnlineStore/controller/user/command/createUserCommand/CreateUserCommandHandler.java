dc7fe08427f01419cf330820a7882eab96dfa8ad
Ekaterina Petukhova <katepthv12@gmail.com>
2024-08-23 11:58:06 +0200

    fix: delete MailServise because of incorrect working

--- backend/src/main/java/com/perfumeOnlineStore/controller/user/command/createUserCommand/CreateUserCommandHandler.java
+++ backend/src/main/java/com/perfumeOnlineStore/controller/user/command/createUserCommand/CreateUserCommandHandler.java
@@ -1,11 +1,8 @@
 package com.perfumeOnlineStore.controller.user.command.createUserCommand;
 
 import an.awesome.pipelinr.Command;
-import com.perfumeOnlineStore.dto.UserDto;
 import com.perfumeOnlineStore.entity.User;
 import com.perfumeOnlineStore.mapper.user.CreateUserCommandToUserMapper;
-import com.perfumeOnlineStore.service.JwtService;
-import com.perfumeOnlineStore.service.MailService;
 import com.perfumeOnlineStore.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +17,6 @@ public class CreateUserCommandHandler implements Command.Handler<CreateUserComma
     private final UserService userService;
     private final CreateUserCommandValidator validator;
     private final PasswordEncoder passwordEncoder;
-    private final MailService mailService;
 
     public CreateUserCommandResponse handle(CreateUserCommand command) {
         CreateUserCommandResponse resp = new CreateUserCommandResponse();
@@ -37,8 +33,6 @@ public class CreateUserCommandHandler implements Command.Handler<CreateUserComma
             resp.setStatusCode(HttpStatus.CREATED.value());
             resp.setStatus(HttpStatus.CREATED.name());
             resp.setPayload(user);
-
-            mailService.sendMail("ekpetukh0va@yandex.ru", "Hello", "You re sign up!");
 
             return resp;
         } catch (Exception e) {
