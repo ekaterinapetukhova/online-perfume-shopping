82e430d22b3ee745c7f5cf6a5c1b3830abb49303
Ekaterina Petukhova <katepthv12@gmail.com>
2024-08-26 11:17:18 +0200

    feat: implement mail entity

---
+++ backend/src/main/java/com/perfumeOnlineStore/entity/Mail.java
@@ -0,0 +1,28 @@
+package com.perfumeOnlineStore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String recipient;
+    @Column(nullable = false)
+    private String subject;
+    @Column(nullable = false)
+    private String controller;
+    @Column(nullable = false)
+    private String action;
+    @Column(nullable = false)
+    private boolean isSentSuccessfully;
+}
